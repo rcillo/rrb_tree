9e3f3f8c996df777a32c8f9cb841a4260af492ff
Ricardo de Cillo <ricardo.cillo@plataformatec.com.br>
2014-08-08 11:55:23 -0300

    Index operation

--- lib/rrb_tree.ex
+++ lib/rrb_tree.ex
@@ -1,2 +1,84 @@
 defmodule RrbTree do
+  # Relaxed Radix Tree
+  #
+  # A datastructure that implements indexed collection operations like
+  # add, index, update, merges and splits in logarithmic time.
+  #
+  # A tree is consisted of one Root, many Internal and Leaf  nodes.
+  #
+  # Root:
+  #
+  # {
+  #   h,       # Integer: the tree height. Leaf nodes have height = 1.
+  #   m,       # Integer: tree branching exponent, as in 2^m
+  #   slots,   # Tuple: number of items in each branch.
+  #   branches # Tuple: branches.
+  # }
+  #
+  # Internal:
+  #
+  # Almost like a Root, but without the branching_size, since it would be
+  # waist of space to store the same information all over the internal nodes.
+  #
+  # {
+  #   slots,
+  #   branches
+  # }
+  #
+  # Leaf:
+  #
+  # A tuple with m items. Theres no need to store `slots` since
+  # it obviously has no branches.
+
+  use Bitwise, only_operators: true
+
+  def get({ h, m, slots, branches }, index) do
+    do_get(h, m, { slots, branches }, index)
+  end
+
+  defp do_get(h, m, { slots, branches }, i) do
+    radix = do_radix(i, m, h)
+    branch_index = do_find_branch_index(slots, radix, i)
+    new_index = do_new_index(slots, branch_index, i)
+
+    do_get(h - 1, m, elem(branches, branch_index), new_index)
+  end
+
+  defp do_get(h, m, items, i) do
+    elem(items, i)
+  end
+
+  # Finds the branch in which the index is expected to be found.
+  # Equivalent to, since the branching factor is a power of 2:
+  #
+  # 1. find the number of items in each branch: $n = 2^{m^{h - 1}}$
+  # 2. find the branch index $radix = \lfloor \div{i}{n} \rfloor$
+  #
+  # Bitwise operators are faster than integer arithmetic.
+  defp do_radix(i, m, h) do
+    i >>> (m * (h - 1))
+  end
+
+  # Since our constraints over the branching factor $2^m$ are relaxed,
+  # e.g. we may have both $2^m - 1$ and $2^m$ branching, sometimes
+  # the expected branch calculated with `do_radix` may not be correct,
+  # so here we do a linear search for the correct branch.
+  defp do_find_branch_index(slots, radix, i) do
+    if elem(slots, radix) > i do
+      radix
+    else
+      do_find_branch_index(slots, radix + 1, i)
+    end
+  end
+
+  # In order to recursively get an item from the tree, we need to
+  # adjust the index, reducing it by the number of items to the
+  # left of the branch we are going to search.
+  defp do_new_index(slots, branch_index, i) do
+    if branch_index == 0 do
+     i
+    else
+      i - elem(slots, branch_index - 1)
+    end
+  end
 end
--- test/rrb_tree_test.exs
+++ test/rrb_tree_test.exs
@@ -1,7 +1,82 @@
 defmodule RrbTreeTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "index by radix" do
+    t = {
+      2,
+      2,
+      {4, 8, 12, 16},
+      {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+      }
+    }
+
+    assert RrbTree.get(t, 5) == 6
+  end
+
+  test "index is the first item in a leaf" do
+    t = {
+      2,
+      2,
+      {4, 8, 12, 16},
+      {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+      }
+    }
+
+    assert RrbTree.get(t, 0) == 1
+    assert RrbTree.get(t, 4) == 5
+  end
+
+  test "index with internal nodes and slot misses" do
+    t = {
+      3,
+      2,
+      {15, 31},
+      {
+        {
+          {4, 8, 12, 15},
+          {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15}
+          }
+        },
+        {
+          {4, 8, 12, 16},
+          {
+            {16, 17, 18, 19},
+            {20, 21, 22, 23},
+            {24, 25, 26, 27},
+            {28, 29, 30, 31}
+          }
+        }
+      }
+    }
+
+    assert RrbTree.get(t, 15) == 16
+  end
+
+  test "linear search fallback when radix miss" do
+    t = {
+      2,
+      2,
+      {3, 6, 10, 11},
+      {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9, 10},
+        {11}
+      }
+    }
+
+    assert RrbTree.get(t, 3) == 4
   end
 end
