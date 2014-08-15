1e6041c47c758c3bfc945ae16d1c7baec93aaa0b
Ricardo de Cillo <ricardo.cillo@plataformatec.com.br>
2014-08-14 23:37:10 -0300

    Using structs for improved clarity
    Also defining the branching factor as module variable, since it would not be possible
    to handle different branching factors across all operations.

--- lib/rrb_tree.ex
+++ lib/rrb_tree.ex
@@ -1,51 +1,39 @@
 defmodule RrbTree do
+  @moduledoc """
   # Relaxed Radix Tree
   #
   # A datastructure that implements indexed collection operations like
   # add, index, update, merges and splits in logarithmic time.
   #
-  # A tree is consisted of one Root, many Internal and Leaf  nodes.
-  #
-  # Root:
-  #
-  # {
-  #   h,       # Integer: the tree height. Leaf nodes have height = 1.
-  #   m,       # Integer: tree branching exponent, as in 2^m
-  #   slots,   # Tuple: number of items in each branch.
-  #   branches # Tuple: branches.
-  # }
-  #
-  # Internal:
-  #
-  # Almost like a Root, but without the branching_size, since it would be
-  # waist of space to store the same information all over the internal nodes.
-  #
-  # {
-  #   slots,
-  #   branches
-  # }
-  #
-  # Leaf:
-  #
-  # A tuple with m items. Theres no need to store `slots` since
-  # it obviously has no branches.
+  # A tree is consisted of one Root, many internal Nodes and Leaf nodes.
+  """
+
+  @m 2 # Integer: tree branching exponent, as in 2^m. E.g, m = 2 so branching factor of 4.
+
+  defstruct h: nil,   # the tree height. Leaf nodes have height = 1.
+            node: nil
+
+  defmodule Node do
+    defstruct ranges: nil,
+              slots: nil
+  end
 
   use Bitwise, only_operators: true
 
-  def get({ h, m, slots, branches }, index) do
-    do_get(h, m, { slots, branches }, index)
+  def get(%RrbTree{} = t, index) do
+    do_get(t.h, t.node, index)
   end
 
-  defp do_get(h, m, { slots, branches }, i) do
-    radix = do_radix(i, m, h)
-    branch_index = do_find_branch_index(slots, radix, i)
-    new_index = do_new_index(slots, branch_index, i)
+  defp do_get(h, %Node{} = node, i) do
+    radix = do_radix(i, h)
+    branch_index = do_find_branch_index(node.ranges, radix, i)
+    new_index = do_new_index(node.ranges, branch_index, i)
 
-    do_get(h - 1, m, elem(branches, branch_index), new_index)
+    do_get(h - 1, elem(node.slots, branch_index), new_index)
   end
 
-  defp do_get(h, m, items, i) do
-    elem(items, i)
+  defp do_get(_h = 1, leaf, i) do
+    elem(leaf, i)
   end
 
   # Finds the branch in which the index is expected to be found.
@@ -55,8 +43,8 @@ defmodule RrbTree do
   # 2. find the branch index $radix = \lfloor \div{i}{n} \rfloor$
   #
   # Bitwise operators are faster than integer arithmetic.
-  defp do_radix(i, m, h) do
-    i >>> (m * (h - 1))
+  defp do_radix(i, h) do
+    i >>> (@m * (h - 1))
   end
 
   # Since our constraints over the branching factor $2^m$ are relaxed,
@@ -74,11 +62,11 @@ defmodule RrbTree do
   # In order to recursively get an item from the tree, we need to
   # adjust the index, reducing it by the number of items to the
   # left of the branch we are going to search.
-  defp do_new_index(slots, branch_index, i) do
+  defp do_new_index(ranges, branch_index, i) do
     if branch_index == 0 do
      i
     else
-      i - elem(slots, branch_index - 1)
+      i - elem(ranges, branch_index - 1)
     end
   end
 end
--- test/rrb_tree_test.exs
+++ test/rrb_tree_test.exs
@@ -1,16 +1,17 @@
 defmodule RrbTreeTest do
   use ExUnit.Case
+  alias RrbTree.Node, as: Node
 
   test "index by radix" do
-    t = {
-      2,
-      2,
-      {4, 8, 12, 16},
-      {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
+    t = %RrbTree{h: 2,
+      node: %Node{
+        ranges: {4, 8, 12, 16},
+        slots: {
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 16}
+        }
       }
     }
 
@@ -18,15 +19,16 @@ defmodule RrbTreeTest do
   end
 
   test "index is the first item in a leaf" do
-    t = {
-      2,
-      2,
-      {4, 8, 12, 16},
-      {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
+    t = %RrbTree{
+      h: 2,
+      node: %Node{
+        ranges: {4, 8, 12, 16},
+        slots: {
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 16}
+        }
       }
     }
 
@@ -35,46 +37,44 @@ defmodule RrbTreeTest do
   end
 
   test "index with internal nodes and slot misses" do
-    t = {
-      3,
-      2,
-      {15, 31},
-      {
-        {
-          {4, 8, 12, 15},
-          {
+    t = %RrbTree{
+      h: 3,
+      node: %Node{ ranges: {15, 31},
+      slots: {
+        %Node{
+          ranges: {4, 8, 12, 15},
+          slots: {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
             {9, 10, 11, 12},
             {13, 14, 15}
           }
         },
-        {
-          {4, 8, 12, 16},
-          {
+        %Node{
+          ranges: {4, 8, 12, 16},
+          slots: {
             {16, 17, 18, 19},
             {20, 21, 22, 23},
             {24, 25, 26, 27},
             {28, 29, 30, 31}
           }
         }
-      }
+      }}
     }
 
     assert RrbTree.get(t, 15) == 16
   end
 
   test "linear search fallback when radix miss" do
-    t = {
-      2,
-      2,
-      {3, 6, 10, 11},
-      {
+    t = %RrbTree{
+      h: 2,
+      node: %Node{ranges: {3, 6, 10, 11},
+      slots: {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9, 10},
         {11}
-      }
+      }}
     }
 
     assert RrbTree.get(t, 3) == 4
